d0d6eddf3114b08f6a970bd23ab108e9fdc0e9ba
Jorge Díaz <jorgeandresdn1@gmail.com>
2017-06-12 14:57:04 -0500

    Add files via upload

---
+++ Problem417UVA.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Hashtable;
+
+class Problem417UVA {
+
+	static Hashtable<String, Integer> memo = new Hashtable<>();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		StringBuilder base = new StringBuilder("a");
+		memo.put(base.toString(), 1);
+
+		int ultimaPosicion = base.length() - 1;
+		int count = 1;
+
+		while (count < 83681) {
+
+			if (ultimoPatron(base) == base.length()) {
+				base.insert(0, (char) ('a' - 1));
+				modificarString(base, 0);
+				ultimaPosicion++;
+			} else if (base.charAt(base.length() - 1) == 'z') {
+				modificarString(base, (base.length() - 1) - (ultimoPatron(base)));
+			} else {
+				base.setCharAt(ultimaPosicion, (char) (base.charAt(ultimaPosicion) + 1));
+			}
+
+			memo.put(base.toString(), ++count);
+
+		}
+
+		String cadena;
+		while ((cadena = in.readLine()) != null) {
+			System.out.println(stringCorrecto(cadena) ? new Integer(memo.get(cadena)) : 0);
+		}
+	}
+
+	static void modificarString(StringBuilder base, int indice) {
+
+		base.setCharAt(indice, (char) (base.charAt(indice) + 1));
+		for (int i = indice + 1; i < base.length(); i++) {
+			base.setCharAt(i, (char) (base.charAt(i - 1) + 1));
+		}
+
+	}
+
+	static int ultimoPatron(StringBuilder base) {
+
+		int count = 0;
+		for (int i = base.length() - 1, j = 0; i >= 0; i--, j++) {
+			if ((int) base.charAt(i) == 122 - j)
+				count++;
+		}
+
+		return count;
+	}
+
+	static boolean stringCorrecto(String palabra) {
+
+		for (int i = 1; i < palabra.length(); i++) {
+			if (palabra.charAt(i - 1) >= palabra.charAt(i))
+				return false;
+		}
+
+		return true;
+	}
+
+}
