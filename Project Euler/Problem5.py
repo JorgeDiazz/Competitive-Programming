a8bf7960bb0eadaab3898f2f994a2864494b5481
Jorge Díaz <jorgeandresdn1@gmail.com>
2017-07-02 11:41:02 -0500

    Create Problem5.py

---
+++ Project Euler/Problem5.py
@@ -0,0 +1,4 @@
+num = 100000
+while num%2 != 0 or num%3 != 0 or num%4 != 0 or num%5 != 0 or num%6 != 0 or num%7 != 0 or num%8 != 0 or num%9 != 0 or num%10 != 0 or num%11 != 0 or num%12 != 0 or num%13 != 0 or num%14 != 0 or num%15 != 0 or num%16 != 0 or num%17 != 0 or num%18 != 0 or num%19 != 0 or num%20 != 0:
+	num += 1	
+print(num)
