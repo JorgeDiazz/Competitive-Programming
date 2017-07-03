c62b3b421385ae8266c3ce34275681cdf12115cc
Jorge Díaz <jorgeandresdn1@gmail.com>
2017-07-02 20:42:03 -0500

    Create Problem11.py

---
+++ Project Euler/Problem11.py
@@ -0,0 +1,45 @@
+matrix = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
+
+for x in range(0,20): matrix[x] = input().split()
+
+result = 0; currentResult = 1
+
+#Left to Right
+for row in matrix:
+    for start in range(17):
+        for index in range(start, start + 4):
+            currentResult *= int(row[index])
+        if currentResult > result:
+            result = currentResult
+        currentResult = 1
+
+#Up to Down
+for column in range(0,20):
+    for row in range(0,17):
+        for rowIterate in range(row, row + 4):
+            currentResult *= int(matrix[rowIterate][column])
+        if currentResult > result:
+            result = currentResult
+        currentResult = 1        
+
+#Left to Rigth Diagonal
+for row in range(0,17):
+    for index in range(0,17):
+        currentResult *= int(matrix[row][index]) * int(matrix[row + 1][index + 1]) * int(matrix[row + 2][index + 2]) * int(matrix[row + 3][index + 3])
+        if currentResult > result:
+            result = currentResult
+        currentResult = 1                
+
+#Rigth to Left Diagonal
+column = 19; row = 0
+while column > 2:
+    row = 0
+    while row < 17:
+        currentResult *= int(matrix[row][column]) * int(matrix[row + 1][column - 1]) * int(matrix[row + 2][column - 2]) * int(matrix[row + 3][column - 3])
+        if currentResult > result:
+            result = currentResult
+        currentResult = 1
+        row += 1                
+    column -= 1    
+    
+print(result)
