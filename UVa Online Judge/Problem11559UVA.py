b6695ff073b55fa407a5afa9b0744a7f74e9ccad
Jorge Díaz <jorgeandresdn1@gmail.com>
2017-07-14 14:34:03 -0500

    Update Problem11559UVA.py

--- UVa Online Judge/Problem11559UVA.py
+++ UVa Online Judge/Problem11559UVA.py
@@ -1,16 +1,16 @@
 while True:
     try:
-        num_participantes, presupuesto, num_hoteles, fines_semana = map(int, input().split())
+        datos = list(map(int, input().split()))
         REF = 1 << 30; costo_minimo = REF 
     except:
         break
 
-    for x in range(num_hoteles):
+    for x in range(datos[2]):
         costo_hotel = int(input())
         camas_disponibles = map(int, input().split())
         for num_camas in camas_disponibles:
-            costo = num_participantes * costo_hotel
-            if num_camas >= num_participantes and costo <= presupuesto:
+            costo = datos[0] * costo_hotel
+            if num_camas >= datos[0] and costo <= datos[1]:
                 costo_minimo = min(costo_minimo, costo)
 
     print(costo_minimo if costo_minimo != REF  else "stay home")
