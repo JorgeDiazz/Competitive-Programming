cd7108476e7c79228b74c457e3c0093cfb2fc2d2
Jorge Díaz <jorgeandresdn1@gmail.com>
2017-09-13 14:08:52 -0500

    Update PrintArray.java

--- ADT/PrintArray.java
+++ ADT/PrintArray.java
@@ -2,12 +2,14 @@ import java.util.Arrays;
 
 public class PrintArray {
 
+	// Prints a vector
 	public static void printVector(String nameVector, Object[] vector) {
 		System.out.println("Vector " + nameVector);
 		Arrays.stream(vector).forEach(cell -> System.out.print(cell + " "));
 		System.out.println();
 	}
 
+	// Prints a matrix
 	public static void printMatrix(String nameMatrix, Object[][] matrix) {
 		System.out.println("Matrix " + nameMatrix);
 		Arrays.stream(matrix).forEach((Object row[]) -> {
