1208c51d97a104f7131a51b0bcb8753dc3b736c1
Jorge Díaz <jorgeandresdn1@gmail.com>
2017-09-13 13:55:14 -0500

    Add files via upload

---
+++ ADT/PrintArray.java
@@ -0,0 +1,22 @@
+package utilities;
+
+import java.util.Arrays;
+
+public class PrintArray {
+
+	public static void printVector(String nameVector, Object[] vector) {
+		System.out.println("Vector " + nameVector);
+		Arrays.stream(vector).forEach(cell -> System.out.print(cell + " "));
+		System.out.println();
+	}
+
+	public static void printMatrix(String nameMatrix, Object[][] matrix) {
+		System.out.println("Matrix " + nameMatrix);
+		Arrays.stream(matrix).forEach((Object row[]) -> {
+			Arrays.stream(row).forEach(cell -> System.out.print(cell + " "));
+			System.out.println();
+		});
+		System.out.println();
+	}
+
+}
