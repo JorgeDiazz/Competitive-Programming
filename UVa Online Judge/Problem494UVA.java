207a05147f552ab017dedb6ac63315630bc8d7c8
Jorge Díaz <jorgeandresdn1@gmail.com>
2017-12-09 19:17:56 -0500

    Create Problem494UVA.java

---
+++ UVa Online Judge/Problem494UVA.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Problem494UVA {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		String input;
+		while ((input = in.readLine()) != null) {
+
+			int words = 0;
+			for (String string : input.split("[\\W\\d]"))
+				for (char letter : string.toCharArray())
+					if (Character.isAlphabetic(letter)) {
+						words++;
+						break;
+					}
+
+			System.out.println(words);
+
+		}
+
+	}
+
+}
