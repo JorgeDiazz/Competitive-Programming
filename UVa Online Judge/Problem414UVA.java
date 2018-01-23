2e54f620d57783e29d7167bd7eef317f61e2e132
Jorge Díaz <jorgeandresdn1@gmail.com>
2018-01-22 19:35:58 -0500

    Add files via upload

---
+++ UVa Online Judge/Problem414UVA.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Problem414UVA {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		String input;
+		while ((input = in.readLine()).compareTo("0") != 0) {
+
+			int minSpaces = Integer.MAX_VALUE;
+			String[] rows = new String[Integer.parseInt(input)];
+			int[] counts = new int[rows.length];
+
+			for (int i = 0; i < rows.length; i++) {
+				rows[i] = in.readLine();
+				counts[i] = countSpaces(rows[i]);
+				minSpaces = Math.min(minSpaces, counts[i]);
+			}
+
+			
+			int voids = 0;
+			for (int B : counts) voids += B - minSpaces; 
+			System.out.println(voids);
+			
+			
+
+		}
+
+	}
+
+	private static int countSpaces(String row) {
+
+		int count = 0;
+		for (char character : row.toCharArray()) {
+			if (character == ' ') count++;
+			else if (count > 0)	return count;
+		}
+
+		return 0;
+	}
+	
+	
+	
+
+}
