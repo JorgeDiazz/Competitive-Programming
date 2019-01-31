0e8bf6004e2236eafcfa5ef936efdb80259f97d9
Jorge Díaz <jorgeandresdn1@gmail.com>
2019-01-30 22:51:31 -0500

    Add files via upload

--- UVa Online Judge/Problem12405UVA.java
+++ UVa Online Judge/Problem12405UVA.java
@@ -10,25 +10,14 @@ class Problem12405UVA {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		char[] field;
 		int totalCases = Integer.parseInt(in.readLine());
-		for (int currentCase = 1, totalScarecrow, lastCrop; currentCase <= totalCases; currentCase++) {
+		for (int currentCase = 1, totalScarecrow; currentCase <= totalCases; currentCase++) {
 			in.readLine(); // dummy read
 			field = in.readLine().toCharArray();
 			totalScarecrow = 0;
-			lastCrop = field.length - 1;
 			for (int index = 0; index < field.length; index++) {
 				if (field[index] == POINT) {
-					index++;
+					index += 2;
 					totalScarecrow++;
-				} else {
-					if (index < lastCrop - 1 && field[index + 1] == POINT) {
-						index += 2;
-						totalScarecrow++;
-					} else if (index < lastCrop && field[index + 1] == POINT) {
-						totalScarecrow++;
-					}
-				}
-				if (index < lastCrop && field[index + 1] == POINT) {
-					index++;
 				}
 			}
 			System.out.println("Case " + currentCase + ": " + totalScarecrow);
